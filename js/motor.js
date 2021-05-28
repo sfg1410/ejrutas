49f2b64d5a91e88ccd028ec39c5bfac9eb5804ec
SERGIO_FERNANDEZ <SERGIO.FG14@GMAIL.COM>
2021-05-28 14:12:04 +0200

    first commit

---
+++ js/motor.js
@@ -0,0 +1,544 @@
+var experiencia0=[
+{
+    foto:"img/1.png",
+    titulo:" Misterios y Leyendas",
+    detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+    tiempo:120,
+    distancia:2,
+    accesible: true,
+    mascotas:true,
+    fecha:"2020-4- 3",
+    precio:12,
+    maxgrupo:10
+
+},
+{
+    foto:"img/1.png",
+    titulo:" Isla del Chamelo",
+    detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+    tiempo:120,
+    distancia:2,
+    accesible: true,
+    mascotas:true,
+    fecha:"2020-4- 3",
+    precio:10,
+    maxgrupo:4
+
+},
+{
+    foto:"img/1.png",
+    titulo:" La Inquisición",
+    detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+    tiempo:120,
+    distancia:2,
+    accesible: false,
+    mascotas:true,
+    fecha:"2020-4- 3",
+    precio:11,
+    maxgrupo:13
+
+},
+{
+    foto:"img/1.png",
+    titulo:" Cuevas y Subterráneos",
+    detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+    tiempo:120,
+    distancia:2,
+    accesible: true,
+    mascotas:false,
+    fecha:"2020-4- 3",
+    precio:15,
+    maxgrupo:20
+
+},
+
+];
+var experiencia1=[
+    {
+        foto:"img/1.png",
+        titulo:" Gymkana",
+        detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+        tiempo:120,
+        distancia:2,
+        accesible: true,
+        mascotas:true,
+        fecha:"2020-4- 3",
+        precio:12,
+        maxgrupo:10
+    
+    },
+    {
+        foto:"img/1.png",
+        titulo:" Teatro de marionetas",
+        detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+        tiempo:120,
+        distancia:2,
+        accesible: true,
+        mascotas:true,
+        fecha:"2020-4- 3",
+        precio:10,
+        maxgrupo:4
+    
+    },
+    {
+        foto:"img/1.png",
+        titulo:" Tarde de Museos",
+        detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+        tiempo:120,
+        distancia:2,
+        accesible: false,
+        mascotas:true,
+        fecha:"2020-4- 3",
+        precio:11,
+        maxgrupo:13
+    
+    },
+    {
+        foto:"img/1.png",
+        titulo:" Conciertos al aire libre",
+        detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+        tiempo:120,
+        distancia:2,
+        accesible: true,
+        mascotas:false,
+        fecha:"2020-4- 3",
+        precio:15,
+        maxgrupo:20
+    
+    },
+    
+    ];
+    var experiencia2=[
+        {
+            foto:"img/1.png",
+            titulo:" Fauna en la portiña",
+            detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+            tiempo:120,
+            distancia:2,
+            accesible: true,
+            mascotas:true,
+            fecha:"2020-4- 3",
+            precio:12,
+            maxgrupo:10
+        
+        },
+        {
+            foto:"img/1.png",
+            titulo:" El rio tajo en familia",
+            detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+            tiempo:120,
+            distancia:2,
+            accesible: true,
+            mascotas:true,
+            fecha:"2020-4- 3",
+            precio:10,
+            maxgrupo:4
+        
+        },
+        // {
+        //     foto:"img/1.png",
+        //     titulo:" La Inquisición",
+        //     detalle:" ksjdkjh lkdjdkljl lñsksjslkd lkjf",
+        //     tiempo:120,
+        //     distancia:2,
+        //     accesible: false,
+        //     mascotas:true,
+        //     fecha:"2020-4- 3",
+        //     precio:11,
+        //     maxgrupo:13
+        
+        // },
+        // {
+        //     foto:"img/1.png",
+        //     titulo:" Cuevas y Subterráneos",
+        //     detalle:" ksjdkjh lkdjdkljl lñsksjslkd lkjf",
+        //     tiempo:120,
+        //     distancia:2,
+        //     accesible: true,
+        //     mascotas:false,
+        //     fecha:"2020-4- 3",
+        //     precio:15,
+        //     maxgrupo:20
+        
+        // },
+        
+        ];
+        var experiencia3=[
+            {
+                foto:"img/1.png",
+                titulo:" Tour turístico por Talavera",
+                detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+                tiempo:120,
+                distancia:2,
+                accesible: true,
+                mascotas:true,
+                fecha:"2020-4- 3",
+                precio:12,
+                maxgrupo:10
+            
+            },
+            {
+                foto:"img/1.png",
+                titulo:" Visita a las iglesias",
+                detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+                tiempo:120,
+                distancia:2,
+                accesible: true,
+                mascotas:true,
+                fecha:"2020-4- 3",
+                precio:10,
+                maxgrupo:4
+            
+            },
+            {
+                foto:"img/1.png",
+                titulo:" Un día siendo alfarero",
+                detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+                tiempo:120,
+                distancia:2,
+                accesible: false,
+                mascotas:true,
+                fecha:"2020-4- 3",
+                precio:11,
+                maxgrupo:13
+            
+            },
+            // {
+            //     foto:"img/1.png",
+            //     titulo:" Cuevas y Subterráneos",
+            //     detalle:" ksjdkjh lkdjdkljl lñsksjslkd lkjf",
+            //     tiempo:120,
+            //     distancia:2,
+            //     accesible: true,
+            //     mascotas:false,
+            //     fecha:"2020-4- 3",
+            //     precio:15,
+            //     maxgrupo:20
+            
+            // },
+            
+            ];
+            var experiencia4=[
+                {
+                    foto:"img/3.png",
+                    titulo:" Las Tapas de la citi",
+                    detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+                    tiempo:120,
+                    distancia:2,
+                    accesible: true,
+                    mascotas:false,
+                    fecha:"2020-4- 3",
+                    precio:20,
+                    maxgrupo:10
+                
+                },
+                {
+                    foto:"img/3.png",
+                    titulo:" Degustación de jamón",
+                    detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+                    tiempo:120,
+                    distancia:2,
+                    accesible: true,
+                    mascotas:false,
+                    fecha:"2020-4- 3",
+                    precio:10,
+                    maxgrupo:4
+                
+                },
+                {
+                    foto:"img/3.png",
+                    titulo:" De copas por la citi",
+                    detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+                    tiempo:120,
+                    distancia:2,
+                    accesible: true,
+                    mascotas:false,
+                    fecha:"2020-4- 3",
+                    precio:11,
+                    maxgrupo:13
+                
+                },
+                {
+                    foto:"img/3.png",
+                    titulo:" Comunidad 3 culturas",
+                    detalle:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum aliquid optio nesciunt iste, possimus, porro facere similique distinctio quod, quaerat id repudiandae. Deleniti, culpa pariatur sint velit quis provident?",
+
+                    tiempo:120,
+                    distancia:2,
+                    accesible: false,
+                    mascotas:false,
+                    fecha:"2020-4- 3",
+                    precio:15,
+                    maxgrupo:20
+                
+                },
+                
+                ];
+miexperiencia0(0);
+function miexperiencia0(x) {
+    document.getElementById('container').innerHTML=
+    `  <div class="cajas">
+<img src="${experiencia0[x].foto}" id="foto">
+</div>
+<div class="cajas">
+<h2>${experiencia0[x].titulo}</h2>
+<h4>${experiencia0[x].detalle}</h4>
+<input type="date" name="" id="">
+<div class="elementos">
+    <div class="redondo">${experiencia0[x].tiempo}min</div>
+    <div class="redondo">${experiencia0[x].distancia}</div>
+   ${accesible(`${x}`)}
+    ${mascotas(`${x}`)}
+</div>
+<div class="cantidad">
+    <div id="precio">${experiencia0[x].precio}</div><span>€</span>
+    
+    <div onclick="menos(${x})">-</div>
+    <div id="cantidad">1</div>
+    <div onclick="mas(${x})">+</div>
+    <div>Personas</div>
+</div>
+          <button onclick="">Comprar</button>  
+</div>
+
+<div class="cajas">
+<div class="ruta" onclick="miexperiencia0(0)">Leyendas y Misterios</div>
+<div class="ruta" onclick="miexperiencia0(1)">Isla del Chamelo</div>
+<div class="ruta" onclick="miexperiencia0(2)">La inquisicion</div>
+<div class="ruta" onclick="miexperiencia0(3)">Cuevas y Subterráneos</div>
+</div>
+`
+    
+};
+
+function miexperiencia1(x) {
+    document.getElementById('container').innerHTML=
+    `  <div class="cajas">
+<img src="${experiencia1[x].foto}" id="foto">
+</div>
+<div class="cajas">
+<h2>${experiencia1[x].titulo}</h2>
+<h4>${experiencia1[x].detalle}</h4>
+<input type="date" name="" id="">
+<div class="elementos">
+    <div class="redondo">${experiencia1[x].tiempo}</div>
+    <div class="redondo">${experiencia1[x].distancia}</div>
+   ${accesible(`${x}`)}
+    ${mascotas(`${x}`)}
+</div>
+<div class="cantidad">
+    <div id="precio">${experiencia1[x].precio}</div>
+    <div>Personas</div>
+    <div onclick="menos(${x})">-</div>
+    <div id="cantidad">1</div>
+    <div onclick="mas(${x})">+</div>
+</div>
+          <button onclick="">Comprar</button>  
+</div>
+
+<div class="cajas">
+<div class="ruta" onclick="miexperiencia1(0)">Gymkana</div>
+<div class="ruta" onclick="miexperiencia1(1)">Teatro de marionetas</div>
+<div class="ruta" onclick="miexperiencia1(2)">Tarde de museos</div>
+<div class="ruta" onclick="miexperiencia1(3)">Conciertos al aire libre</div>
+</div>
+`
+    
+}
+
+function miexperiencia2(x) {
+    document.getElementById('container').innerHTML=
+    `  <div class="cajas">
+<img src="${experiencia2[x].foto}" id="foto">
+</div>
+<div class="cajas">
+<h2>${experiencia2[x].titulo}</h2>
+<h4>${experiencia2[x].detalle}</h4>
+<input type="date" name="" id="">
+<div class="elementos">
+    <div class="redondo">${experiencia2[x].tiempo}</div>
+    <div class="redondo">${experiencia2[x].distancia}</div>
+   ${accesible(`${x}`)}
+    ${mascotas(`${x}`)}
+</div>
+<div class="cantidad">
+    <div id="precio">${experiencia2[x].precio}</div>
+    <div>Personas</div>
+    <div onclick="menos(${x})">-</div>
+    <div id="cantidad">1</div>
+    <div onclick="mas(${x})">+</div>
+</div>
+          <button onclick="">Comprar</button>  
+</div>
+
+<div class="cajas">
+<div class="ruta" onclick="miexperiencia2(0)">Fauna en la portiña</div>
+<div class="ruta" onclick="miexperiencia2(1)">El rio tajo en familia</div>
+
+</div>
+`
+    
+}
+
+function miexperiencia3(x) {
+    document.getElementById('container').innerHTML=
+    `  <div class="cajas">
+<img src="${experiencia3[x].foto}" id="foto">
+</div>
+<div class="cajas">
+<h2>${experiencia3[x].titulo}</h2>
+<h4>${experiencia3[x].detalle}</h4>
+<input type="date" name="" id="">
+<div class="elementos">
+    <div class="redondo">${experiencia3[x].tiempo}</div>
+    <div class="redondo">${experiencia3[x].distancia}</div>
+   ${accesible(`${x}`)}
+    ${mascotas(`${x}`)}
+</div>
+<div class="cantidad">
+    <div id="precio">${experiencia3[x].precio}</div>
+    <div>Personas</div>
+    <div onclick="menos(${x})">-</div>
+    <div id="cantidad">1</div>
+    <div onclick="mas(${x})">+</div>
+</div>
+          <button onclick="">Comprar</button>  
+</div>
+
+<div class="cajas">
+<div class="ruta" onclick="miexperiencia3(0)">Tour turístico por Talavera</div>
+<div class="ruta" onclick="miexperiencia3(1)">Visita a las Iglesias</div>
+<div class="ruta" onclick="miexperiencia3(2)">Un día siendo alfarero</div>
+
+</div>
+`
+    
+}
+
+function miexperiencia4(x) {
+    document.getElementById('container').innerHTML=
+    `  <div class="cajas">
+<img src="${experiencia4[x].foto}" id="foto">
+</div>
+<div class="cajas">
+<h2>${experiencia4[x].titulo}</h2>
+<h4>${experiencia4[x].detalle}</h4>
+<input type="date" name="" id="">
+<div class="elementos">
+    <div class="redondo">${experiencia4[x].tiempo}</div>
+    <div class="redondo">${experiencia4[x].distancia}</div>
+   ${accesible(`${x}`)}
+    ${mascotas(`${x}`)}
+</div>
+<div class="cantidad">
+    <div id="precio">${experiencia4[x].precio}</div>
+    <div>Personas</div>
+    <div onclick="menos(${x})">-</div>
+    <div id="cantidad">1</div>
+    <div onclick="mas(${x})">+</div>
+</div>
+          <button onclick="">Comprar</button>  
+</div>
+
+<div class="cajas">
+<div class="ruta" onclick="miexperiencia4(0)">De tapas por Talaver</div>
+<div class="ruta" onclick="miexperiencia4(1)">Degustación de jamón</div>
+<div class="ruta" onclick="miexperiencia4(2)">De copas por Talavera</div>
+<div class="ruta" onclick="miexperiencia4(3)">Comidas 3 culturas</div>
+
+</div>
+`
+    
+}
+
+
+
+function accesible(x) {
+    if(experiencia0[x].accesible){
+return '<div class="redondo"><img src="img/silla.png" class="icono"></div>'
+}else{
+    return "";
+}
+}
+function mascotas(x) {
+    if(experiencia0[x].mascotas){
+    return  `<div class="redondo"><img src="img/dog.png" class="icono"></div>`
+}else{
+    return "";
+}
+}
+
+function mas(x) {
+   let  vervalor=parseInt(document.getElementById('cantidad').innerHTML);
+    if(experiencia0[x].maxgrupo>vervalor){ 
+    vervalor++
+}
+    document.getElementById('cantidad').innerHTML=vervalor;
+    
+    multiplica(x);
+}
+function menos(x) {
+     let vervalor=parseInt(document.getElementById('cantidad').innerHTML);
+    if(vervalor>1){
+    vervalor--
+    document.getElementById('cantidad').innerHTML=vervalor;
+    }
+    multiplica(x);
+}
+
+function multiplica(x) {
+    let vervalor=parseInt(document.getElementById('cantidad').innerHTML);
+    let verprecio=experiencia0[x].precio;
+    let total =vervalor*verprecio;
+    document.getElementById('precio').innerHTML=total;
+}
+function multiplica(x) {
+    let vervalor=parseInt(document.getElementById('cantidad').innerHTML);
+    let verprecio=experiencia1[x].precio;
+    let total =vervalor*verprecio;
+    document.getElementById('precio').innerHTML=total;
+}
+function multiplica(x) {
+    let vervalor=parseInt(document.getElementById('cantidad').innerHTML);
+    let verprecio=experiencia2[x].precio;
+    let total =vervalor*verprecio;
+    document.getElementById('precio').innerHTML=total;
+}
+function multiplica(x) {
+    let vervalor=parseInt(document.getElementById('cantidad').innerHTML);
+    let verprecio=experiencia3[x].precio;
+    let total =vervalor*verprecio;
+    document.getElementById('precio').innerHTML=total;
+}
+function multiplica(x) {
+    let vervalor=parseInt(document.getElementById('cantidad').innerHTML);
+    let verprecio=experiencia4[x].precio;
+    let total =vervalor*verprecio;
+    document.getElementById('precio').innerHTML=total;
+}
+// function sumar(x) {
+   
+//     let precio=parseInt(document.getElementById(`cantidad${x}`).innerHTML);
+//     precio++;
+//     document.getElementById(`cantidad${x}`).innerHTML=precio;
+// }
+// function restar(x) {
+//     let precio=parseInt(document.getElementById(`cantidad${x}`).innerHTML);
+//     precio--;
+//     document.getElementById(`cantidad${x}`).innerHTML=precio;
+// }
